b1c7fa66e241e90455807e391d85582cf4c343ce
zgx <1046895847@qq.com>
2022-07-27 00:03:36 +0800

    first commit

---
+++ src/main/java/com/myf/SimpleFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.myf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFunctionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myf/SimpleFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFunctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
